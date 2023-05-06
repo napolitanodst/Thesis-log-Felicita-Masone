37f7941f139c0ae76317d177567c7766726e6cdc
Felicita Masone <125889235+Felicita-Mas@users.noreply.github.com>
2023-05-06 12:43:19 +0200

    Update Baseline expr.R

--- Heme-Hemopexin/Baseline expr.R
+++ Heme-Hemopexin/Baseline expr.R
@@ -5,31 +5,31 @@ non0s_x_gene <- rowData(spe_h1000)$no_zero_x_gene
 # before
 counts_matrix <- assays(spe)$counts
 counts_matrix <- as.matrix(counts_matrix)
-average_expr_before <- rowSums(counts_matrix)/non0s_x_gene
-mean_expr_before <- mean(average_expr_before)
+baseline_before <- rowSums(counts_matrix)/non0s_x_gene
+mean_expr_before <- mean(baseline_before)
 
 dist_mean_before <- c()
-for(i in 1:length(average_expr_before)){
-  dist_mean_before[i] = abs(average_expr_before[i] - mean_expr_before)
+for(i in 1:length(baseline_before)){
+  dist_mean_before[i] = abs(baseline_before[i] - mean_expr_before)
 }
 
 rank_baseline_before <- rank(dist_mean_before, ties.method = "min")
 
-rowData(spe)$baseline_before <- average_expr_before
+rowData(spe)$baseline_before <- baseline_before
 rowData(spe)$rank_baseline_before <- rank_baseline_before
 
 # after
 BCcounts <- assays(spe)$BCcounts
 BCcounts <- as.matrix(BCcounts)
-average_expr_after <- rowSums(BCcounts)/non0s_x_gene
-mean_expr_after <- mean(average_expr_after)
+baseline_after <- rowSums(BCcounts)/non0s_x_gene
+mean_expr_after <- mean(baseline_after)
 
 dist_mean_after <- c()
-for(i in 1:length(average_expr_after)){
-  dist_mean_after[i] = abs(average_expr_after[i] - mean_expr_after)
+for(i in 1:length(baseline_after)){
+  dist_mean_after[i] = abs(baseline_after[i] - mean_expr_after)
 }
 
 rank_baseline_after <- rank(dist_mean_after, ties.method = "min")
 
-rowData(spe)$baseline_after <- average_expr_after
+rowData(spe)$baseline_after <- baseline_after
 rowData(spe)$rank_baseline_after <- rank_baseline_after
